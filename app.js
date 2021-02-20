bdc6a08b5af53c49d12815e26c2e705b3de7e54e
kento-yoshidu <kento yoshizu>
2021-02-20 12:45:49 +0900

    Develop app.js

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 const http = require('http');
 const express = require('express');
 const ejs = require('ejs');
-const pg = require('pg');
+const { Client } = require('pg');
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-const pgPool = new pg.Pool({
+const client = new Client({
   database: "bookshelf",
   user: "master",
   password: "Decjapan001",
   port: 11111,
 });
 
-const client = () => {
-  return new Promise((resolve, reject) => {
-    pgPool.connect((err, client) => {
-      if (err) console.log(err)
-
-      resolve(client)
-    })
-  })
-}
-
 app.get('/', (req, res) => {
 
-  client().then(result => {
-    console.log(result)
+  client
+    .connect()
+    .then(() => console.log("connected"))
+    .then(() => client.query("SELECT *  FROM testtable"))
+    .then(result => {
+      console.log(result.rows[0].id)
+      res.render("./index.ejs", {
+        test: result.rows[0].id,
+      });
 
-    res.render("./index.ejs", {
-      test: result.user,
-    });
   })
-})
+});
 
 const server = http.createServer(app);
 
